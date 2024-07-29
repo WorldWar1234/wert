400e8456d820182328a2a576794f05e5bc0ac5ec
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-29 12:20:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const proxy = require('./src/proxy');
 
 const PORT = process.env.PORT || 8080;
 
-app.enable('trust proxy');
+
 app.get('/', authenticate, params, proxy);
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
