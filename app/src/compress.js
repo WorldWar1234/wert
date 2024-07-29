a03fe48ac7148d14327230b64a563d3f65b2e93b
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-29 16:45:16 +0530

    Update compress.js

--- app/src/compress.js
+++ app/src/compress.js
@@ -1,1 +1,20 @@
 
+const sharp = require('sharp');
+
+function compress(req, res, input) {
+  const format = req.params.webp ? 'webp' : 'jpeg';
+
+  return sharp(input)
+    .toFormat(format, {
+      quality: req.params.quality,
+      progressive: true,
+      optimizeScans: true
+    })
+    .toBuffer()
+    .catch(err => {
+      console.error('Error compressing image:', err);
+      return input; // Return the original image buffer
+    });
+}
+
+module.exports = compress;
