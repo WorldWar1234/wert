936ba2e6755a9099b6e7726b101080d1948394e0
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-29 14:16:26 +0530

    Delete src/bypass.js

--- src/bypass.js
+++
@@ -1,7 +0,0 @@
-function bypass(req, res, content) {
-  const buffer = Buffer.from(content);
-  res.setHeader('Content-Length', buffer.length);
-  res.status(200).send(content);
-}
-
-module.exports = bypass;
