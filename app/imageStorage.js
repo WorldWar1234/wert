1fcd5cfd9fddc2108380b0b12bef74829bb669d6
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-29 16:34:32 +0530

    Update imageStorage.js

--- app/imageStorage.js
+++ app/imageStorage.js
@@ -1,1 +1,16 @@
 
+const scalingo = require('scalingo-sdk');
+
+const client = new scalingo.Client({ token:tk-us-yhyRo6DKcM_VJY7gpqXZ6Ni0KHf73apagIADaciq_ntdtBJZ});
+
+async function getImage(imageId) {
+  try {
+    const file = await client.file.download(imageId);
+    return file.content;
+  } catch (err) {
+    console.error('Error fetching image:', err);
+    throw err;
+  }
+}
+
+module.exports = { getImage };
