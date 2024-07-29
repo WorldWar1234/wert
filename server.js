3ab6dd5f24a9c77514f7711a3a18acdcfaec6741
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-29 12:26:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const authenticate = require('./src/authenticate');
 const params = require('./src/params');
 const proxy = require('./src/proxy');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 
 
 app.get('/', authenticate, params, proxy);
